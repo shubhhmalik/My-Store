5ad1f09cd6f5907b527ce81ecb2b7957b8499f0d
Shubh <131964323+shubhhmalik@users.noreply.github.com>
2025-05-10 13:08:25 +0530

    Update AddProductRequest.java

--- my-store/src/main/java/com/project/my_store/requests/AddProductRequest.java
+++ my-store/src/main/java/com/project/my_store/requests/AddProductRequest.java
@@ -7,10 +7,8 @@ import lombok.Data;
 import java.math.BigDecimal;
 import java.util.List;
 
-@Data       //for getters and setters, in entities this is not recommended instead we use @Getter and @Setter
+@Data       
 public class AddProductRequest {
-    //basically a copy of Product, because its better to work somewhere else and not
-    //exactly in one of our main entity, just to be safe
 
     private Long id;
     private String name;

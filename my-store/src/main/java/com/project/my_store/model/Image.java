fb397bd7e33bd7848a0be6d79bb0e11f533d8f9e
Shubh <131964323+shubhhmalik@users.noreply.github.com>
2025-05-10 13:05:48 +0530

    Update Image.java

--- my-store/src/main/java/com/project/my_store/model/Image.java
+++ my-store/src/main/java/com/project/my_store/model/Image.java
@@ -17,7 +17,7 @@ import java.sql.Blob;
 @Entity
 public class Image {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)   //The DB automatically generates a unique value when a new row is added
+    @GeneratedValue(strategy = GenerationType.IDENTITY)   
     private Long id;
     private String fileName;
     private String fileType;
@@ -27,7 +27,7 @@ public class Image {
     private String downloadUrl;
 
 
-    @ManyToOne                              //as one product has many images
+    @ManyToOne                              
     @JoinColumn(name = "product_id")
     private Product product;
 }
